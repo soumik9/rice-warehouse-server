7816bd74fe6f9204678685b07871e423046ec89f
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-05-01 15:22:23 +0600

    all products api route created

--- index.js
+++ index.js
@@ -20,19 +20,41 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ricewarehouse.3kpcy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
+
+/* client.connect(err => {
  // const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
   console.log('db connected');
   client.close();
-});
+}); */
+
+async function run(){
+    try{
+
+        await client.connect();
+        const productCollection = client.db("riceWareHouse").collection("products");
+        
+        
+        // api home routes
+        app.get('/' , (req, res) => {
+            res.send('Rice Warehouse Server Is Ready')
+        })
+
+        // api all products routes
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+    }finally{
+
+    }
+}
+
+run().catch(console.dir);
 
 
-// api routes
-
-app.get('/' , (req, res) => {
-    res.send('Rice Warehouse Server Is Ready')
-})
 
 // port listening
 app.listen(port, () => {
