f9a4b17a935c947f54b1fb5ea1622cf2b6b9758a
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-05-06 03:05:35 +0600

    JWT auth and verification on my products

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -15,8 +17,27 @@ app.use(express.json());
     pass - TbBiOyMXtt439fF2
 */
 
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader){
+        return res.status(401).send({message: 'Unauthorized access'})
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+            return res.status(403).send({message: 'Forbidden access'})
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
+// connect to mongo
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ricewarehouse.3kpcy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -25,9 +46,16 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
 
+        // connect to mongodb collection
         await client.connect();
         const productCollection = client.db("riceWareHouse").collection("products");
-        
+
+        // jwt token AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'});
+            res.send({ accessToken });
+        })
         
         // api homepage
         app.get('/' , (req, res) => {
@@ -104,13 +132,19 @@ async function run(){
             res.send(result);
         })
 
-        // my products
-        app.get('/my-products', async (req, res) => {
+        // my products with JWT verification
+        app.get('/my-products', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            const query = { email: email };
-            const cursor = productCollection.find(query);
-            const myProducts = await cursor.toArray();
-            res.send(myProducts);
+
+            if(email === decodedEmail){
+                const query = { email: email };
+                const cursor = productCollection.find(query);
+                const myProducts = await cursor.toArray();
+                res.send(myProducts);
+            }else{
+                res.status(403).send({ message: 'forbidden access' });
+            }
         })
 
         // products count
