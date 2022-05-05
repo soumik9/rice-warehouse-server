011acf38713ba5efddb274622aa72159d94d79bf
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-05-06 03:31:27 +0600

    JWT auth and verification on my products customized

--- index.js
+++ index.js
@@ -21,16 +21,15 @@ function verifyJWT(req, res, next){
     const authHeader = req.headers.authorization;
 
     if(!authHeader){
-        return res.status(401).send({message: 'Unauthorized access'})
+        return res.status(401).send({message: 'Unauthorized access'});
     }
 
     const token = authHeader.split(' ')[1];
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if(err){
-            return res.status(403).send({message: 'Forbidden access'})
+            return res.status(403).send({message: 'Forbidden access'});
         }
-        console.log('decoded', decoded);
         req.decoded = decoded;
         next();
     })
